1379e1eca390a3dad4c78a77878c0c2729239ca1
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-06 15:51:02 -0300

    feat: Requisito 4, creating generic handle

--- src/App.js
+++ src/App.js
@@ -3,32 +3,47 @@ import Form from './components/Form';
 import Card from './components/Card';
 
 class App extends React.Component {
-  // constructor(props) {
-  //   super(props);
+  constructor(props) {
+    super(props);
 
-  //   this.state = {
-  //     cardName: '',
-  //     cardDescription: '',
-  //     cardAttr1: '',
-  //     cardAttr2: '',
-  //     cardAttr3: '',
-  //     cardImage: '',
-  //     cardRare: '',
-  //     cardTrunfo: '',
-  //     hasTrunfo: '',
-  //     isSaveButtonDisabled: '',
-  //   };
-  // }
+    this.state = {
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '',
+      cardAttr2: '',
+      cardAttr3: '',
+      cardImage: '',
+      cardRare: '',
+      cardTrunfo: false,
+      hasTrunfo: false,
+      isSaveButtonDisabled: false,
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange({ target }) {
+    const { name, value } = target;
+
+    const newValue = target.type === 'checkbox' ? 'checked' : value;
+    this.setState((previousState) => ({
+      ...previousState,
+      [name]: newValue,
+    }));
+  }
 
   render() {
     return (
       <div>
         <h1>Tryunfo </h1>
-        <Form />
+        <Form onInputChange={ this.onInputChange } />
         <Card />
       </div>
     );
   }
 }
+
+// cardName, cardDescription, cardAttr1, cardAttr2,
+//       cardAttr3, cardImage, cardRare, cardTrunfo
 
 export default App;
