34add278790ee013af6d0e64c2334ff96390e114
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-06 22:45:25 -0300

    fix: Requisito 8

--- src/App.js
+++ src/App.js
@@ -96,7 +96,7 @@ class App extends React.Component {
   render() {
     const { cardName, cardDescription, cardAttr1, cardAttr2,
       cardAttr3, cardImage, cardRare, cardTrunfo,
-      hasTrunfo, isSaveButtonDisabled } = this.state;
+      hasTrunfo, isSaveButtonDisabled, cards } = this.state;
     return (
       <div>
         <h1>Tryunfo </h1>
@@ -125,6 +125,20 @@ class App extends React.Component {
           cardTrunfo={ cardTrunfo }
 
         />
+        {
+          cards.map((card) => (
+            <Card
+              key={ card.cardName }
+              cardName={ card.cardName }
+              cardDescription={ card.cardDescription }
+              cardAttr1={ card.cardAttr1 }
+              cardAttr2={ card.cardAttr2 }
+              cardAttr3={ card.cardAttr3 }
+              cardImage={ card.cardImage }
+              cardRare={ card.cardRare }
+              cardTrunfo={ card.cardTrunfo }
+            />))
+        }
       </div>
     );
   }
