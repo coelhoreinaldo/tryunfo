6bc5d2232231053f3788b7002a438a0d9ad8d33c
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-11 12:24:43 -0300

    style: naruto sasuke sakura

--- src/data/card-images.js
+++ src/data/card-images.js
@@ -1,23 +1,10 @@
-// // const cardImages = new Map();
+const cardImages = new Map();
 
-// // // Adicione cada imagem ao Mapa de Módulos
-// // cardImages.map((image) => cardImages.set(`${image}`, require('./card-images/image.jpg')));
-// // cardImages.set('naruto', require('./card-images/naruto.jpg'));
-// // cardImages.set('sasuke', require('./card-images/sasuke.jpg'));
-// // cardImages.set('sakura', require('./card-images/sakura.jpg'));
-// // // Adicione outras imagens conforme necessário
+// Adicione cada imagem ao Mapa de Módulos
 
-// // export default cardImages;
+cardImages.set('naruto', require('./card-images/naruto.jpg'));
+cardImages.set('sasuke', require('./card-images/sasuke.jpg'));
+cardImages.set('sakura', require('./card-images/sakura.jpg'));
+// Adicione outras imagens conforme necessário
 
-// const fs = require('fs');
-
-// const cardImages = new Map();
-
-// // Ler o diretório "card-images" e adicionar cada imagem ao Mapa de Módulos
-// fs.readdirSync('./card-images').forEach((file) => {
-//   const cardName = file.replace('.jpg', ''); // Extrair o nome da carta sem a extensão
-//   const cardImagePath = `./card-images/${file}`;
-//   cardImages.set(cardName, require(cardImagePath));
-// });
-
-// export default cardImages;
+export default cardImages;
--- src/data/deck.js
+++ src/data/deck.js
@@ -30,42 +30,41 @@
 // hashirama
 import cardImages from './card-images';
 
-// const newCard = {
-//   cardName,
-//   cardDescription,
-//   cardImage,
-//   cardRare,
-//   cardAttr1,
-//   cardAttr2,
-//   cardAttr3,
-//   cardTrunfo,
-// };
+const muitoRaro = 'muito raro';
+// const raro = 'raro';
+// const normal = 'normal';
 
 const deck = [
-  { cardName: 'Naruto Uzumaki',
+  {
+    cardName: 'Naruto Uzumaki',
     cardDescription: 'Aldeia da Folha, Hokage',
     cardImage: cardImages.get('naruto'),
-    cardRare: 'muito raro',
+    cardRare: muitoRaro,
     cardAttr1: 90,
     cardAttr2: 50,
     cardAttr3: 70,
-    cardTrunfo: true },
-  // {cardName:'Sasuke Uchiha',
-  //   cardDescription: 'Aldeia da Folha, Nukenin',
-  //   cardImage: ,
-  //   cardRare:,
-  //   cardAttr1:,
-  //   cardAttr2:,
-  //   cardAttr3:,
-  //   cardTrunfo: false},
-  // {cardName:,
-  //   cardDescription:,
-  //   cardImage:,
-  //   cardRare:,
-  //   cardAttr1:,
-  //   cardAttr2:,
-  //   cardAttr3:,
-  //   cardTrunfo: false},
+    cardTrunfo: true,
+  },
+  {
+    cardName: 'Sasuke Uchiha',
+    cardDescription: 'Aldeia da Folha, Nukenin',
+    cardImage: cardImages.get('sasuke'),
+    cardRare: muitoRaro,
+    cardAttr1: 90,
+    cardAttr2: 70,
+    cardAttr3: 50,
+    cardTrunfo: false,
+  },
+  {
+    cardName: 'Sakura Haruno',
+    cardDescription: 'Aldeia da Folha, Jounin',
+    cardImage: cardImages.get('sakura'),
+    cardRare: muitoRaro,
+    cardAttr1: 50,
+    cardAttr2: 70,
+    cardAttr3: 90,
+    cardTrunfo: false,
+  },
   // {cardName:,
   //   cardDescription:,
   //   cardImage:,
