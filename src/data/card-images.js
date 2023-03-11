23a5997ec73ee996b3df9539c031512fdccab87c
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-11 19:04:45 -0300

    feat: naruto characters json

--- src/data/card-images.js
+++ src/data/card-images.js
@@ -5,6 +5,19 @@ const cardImages = new Map();
 cardImages.set('naruto', require('./card-images/naruto.jpg'));
 cardImages.set('sasuke', require('./card-images/sasuke.jpg'));
 cardImages.set('sakura', require('./card-images/sakura.jpg'));
+cardImages.set('kakashi', require('./card-images/kakashi.jpg'));
+cardImages.set('shikamaru', require('./card-images/shikamaru.jpg'));
+cardImages.set('chouji', require('./card-images/chouji.jpg'));
+cardImages.set('ino', require('./card-images/ino.jpg'));
+cardImages.set('asuma', require('./card-images/asuma.jpg'));
+cardImages.set('hinata', require('./card-images/hinata.jpg'));
+cardImages.set('kiba', require('./card-images/kiba.jpg'));
+cardImages.set('shino', require('./card-images/shino.jpg'));
+cardImages.set('kurenai', require('./card-images/kurenai.jpg'));
+cardImages.set('neji', require('./card-images/neji.jpg'));
+cardImages.set('rock-lee', require('./card-images/rock-lee.jpg'));
+cardImages.set('tenten', require('./card-images/tenten.jpg'));
+cardImages.set('gai', require('./card-images/gai.jpg'));
 // Adicione outras imagens conforme necessário
 
 export default cardImages;
