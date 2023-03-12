a437cd98da1979c8f64ba137f30b84fa8434c8c8
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-11 21:02:12 -0300

    feat: footer

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <p>Projeto desenvolvido por Reinaldo Coelho.</p>
+        <p>Imagens dos personagens retiradas do site My Anime List.</p>
+        <p>
+          Todos os personagens da série de mangá e anime Naruto
+          pertencem a editora Shueisha e ao escritor Masashi Kishimoto
+
+        </p>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
